439cb81596ddb0d2c9502f161b0602f315aefe0c
saJeremyQin <343220868@qq.com>
2023-12-29 11:47:34 +1030

    Update the endpoint with db operations

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
-import { cartItems as cartItemsRaw, products as productsRaw } from './temp-data';
 
 const url = 'mongodb+srv://jeremyqinsa:600186Qd%21%21@cluster0.xm7uvh0.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(url);
-
-let cartItems = cartItemsRaw;
-let products = productsRaw;
 
 const app = express();
 app.use(express.json());
@@ -14,9 +10,10 @@ app.use(express.json());
 async function populatedCartArray (cartIds) {
     await client.connect();
     const db = client.db('ECommerceApp-db');
-    // console.log(cartIds);
-
-    return Promise.all(cartIds.map((id) => db.collection('products').findOne({id})));
+    console.log(cartIds);
+    // cocurrently execute multiple promises and wait them to resolve, and will be resolved to an array of documents
+    const promises = Promise.all(cartIds.map((id) => db.collection('products').findOne({id})));
+    return promises;
 }
 
 
@@ -24,41 +21,78 @@ app.get('/products', async (req,res) => {
     await client.connect();
     const db = client.db('ECommerceApp-db');
 
-    products = await db.collection('products').find({}).toArray();
+    const products = await db.collection('products').find({}).toArray();
     res.send(products);
 })
 
 app.get('/users/:userId/cart', async (req,res) => {
-
     await client.connect();
     const db = client.db('ECommerceApp-db');
 
+    // user is an object representing a user document from the "users" collection in MongoDB
     const user = await db.collection('users').findOne({id: req.params.userId});
     const cartArray = await populatedCartArray(user.cartItems);
     
     res.json(cartArray);
 })
 
-app.post('/cart', (req,res) => {
+app.get('/products/:productId', async (req,res) => {
+    await client.connect();
+    const db = client.db('ECommerceApp-db');
+
+    const productId = req.params.productId;
+    const product = await db.collection('products').findOne({id:productId});
+    res.json(product);
+})
+
+
+app.post('/users/:userId/cart', async (req,res) => {
+    const userId = req.params.userId;
     const productId = req.body.id;
-    cartItems.push(productId);
-    const cartArray = populatedCartArray(cartItems);
+
+    await client.connect();
+    const db = client.db('ECommerceApp-db');
+
+    await db.collection('users').updateOne(
+        {
+            id: userId
+        },
+        {
+            $addToSet: 
+            {
+                cartItems: productId
+            }
+        }
+    )
+
+    const user = await db.collection('users').findOne({id: userId});
+    const cartArray = await populatedCartArray(user.cartItems);
     res.json(cartArray);    
 })
 
-app.delete('/cart/:productId', (req,res) => {
+app.delete('/users/:userId/cart/:productId',async (req,res) => {
+    const userId = req.params.userId;
     const productId = req.params.productId;
-    console.log(productId);
-    cartItems = cartItems.filter((id) => id !== productId);
-    const cartArray = populatedCartArray(cartItems);
-    res.json(cartArray);
-});
+    
 
-app.get('/products/:productId', (req,res) => {
-    const productId = req.params.productId;
-    const product = products.find((product) => product.id === productId);
-    res.json(product);
-})
+    await client.connect();
+    const db = client.db('ECommerceApp-db');
+
+    await db.collection('users').updateOne(
+        {
+            id: userId
+        },
+        {
+            $pull:
+            {
+                cartItems:productId
+            }
+        }
+    )
+    const user = await db.collection('users').findOne({id: userId});
+    const cartArray = await populatedCartArray(user.cartItems);
+    res.json(cartArray);    
+});
 
 app.listen(8000, () => {
     console.log('Server is runing on 8000');
